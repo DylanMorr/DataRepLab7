03ae319d6f39d50aeb5bb4af756775731c3c941d
Dylan Morrow <G00365708@gmit.ie>
2021-10-05 18:57:53 +0100

    Lab Completed

--- src/App.js
+++ src/App.js
@@ -16,11 +16,14 @@ class App extends Component {
   // wrap everything in render method
   render() {
     return (
+      // put router tags so we can use the switch statement
       <Router>
         <div className="App">
 
+          {/* Create a navbar for navigating the webpage */}
           <Navbar bg="primary" variant="dark">
 
+            {/* Link all the component pages to the navbar and name accordingly */}
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
@@ -30,14 +33,21 @@ class App extends Component {
 
           </Navbar>
 
+          {/* Setup a switch statement to get the correct paths */}
           <Switch>
+            {/* Sets path of file to /header */}
             <Route path="/header">
+              {/* Displays Header class s*/}
               <Header />
             </Route>
+            {/* Sets path of file to /footer */}
             <Route path="/footer">
+              {/* Displays Footer class s*/}
               <Footer />
             </Route>
+            {/* Sets path of file to / as it is just mainpage */}
             <Route path="/">
+              {/* Displays Content class s*/}
               <Content />
             </Route>
           </Switch>
